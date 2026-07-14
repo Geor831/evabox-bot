4894bd6b58e3b6cb58ce25c1e9b70e753868d366
Geor831 <solovevgeorgij939@gmail.com>
2026-07-14 23:50:37 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+import time
+import requests
+from vk_api import VkApi
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+VK_TOKEN = "vk1.a.gB_E6NmXBEv0nRT58o_22HRpW5hhLvc7TC22VbE1M8KBZPgW7beJfO-DmSqnCNGIdVvQu17WHPKa5teVbQq3z93d-pneW6XkAmMdpNowUViS0P0enWa16qKXfA4HRRCvG74_OriEOAF6mtQeddpjDzDoooIAGWBxu84c-1Aj7wE9sGoOrOdVSS5NvnDSjfc0-QunLDoQdSsSgDFQxkIWgg"
+MANAGER_VK_ID = 598512076
+
+def main():
+    vk_session = VkApi(token=VK_TOKEN)
+    longpoll = VkLongPoll(vk_session)
+    vk = vk_session.get_api()
+    print("✅ Бот запущен на Bothost!")
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            uid = event.user_id
+            msg = event.text.strip()
+            if msg:
+                vk.messages.send(user_id=uid, message="Привет! Я бот на Bothost!", random_id=0)
+
+if __name__ == "__main__":
+    main()
